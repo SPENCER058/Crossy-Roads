fc1035bb8183809d6a6e8a20da70c67aa10695c2
SPENCER058 <galihpanji058@student.uns.ac.id>
2023-04-23 21:38:25 +0700

    Add Particle System

--- Assets/Sripts/Road.cs
+++ Assets/Sripts/Road.cs
@@ -12,12 +12,12 @@ public class Road : Terrain
 
 	private void Start () {
 		if (Random.value > 0.5f) {
-			carSpawnPosition = new Vector3(horizontalSize / 2 + 3, 0, this.transform.position.z);
+			carSpawnPosition = new Vector3(horizontalSize / 2 + 10, 0, this.transform.position.z);
 		
 			carRotation = Quaternion.Euler(0, -90, 0);
 
 		} else {
-			carSpawnPosition = new Vector3(-(horizontalSize / 2 + 3), 0, this.transform.position.z);
+			carSpawnPosition = new Vector3(-(horizontalSize / 2 + 10), 0, this.transform.position.z);
 			carRotation = Quaternion.Euler(0, 90, 0);
 		}
 	}
@@ -28,7 +28,7 @@ public class Road : Terrain
 
 			var car = Instantiate(carPrefab, carSpawnPosition, carRotation);
 
-			car.SetUpDistanceLimit(horizontalSize + 6);
+			car.SetUpDistanceLimit(horizontalSize + 30);
 
 			return;
 		}
